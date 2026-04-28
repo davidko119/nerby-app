18128d9b96b993cfac94c966c4b194bd712f59dd
David Sarlak <david.sarlak1@gmail.com>
2026-04-28 11:58:47 +0200

    Initial Commit

---
+++ nerby app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nerby app
+//
+//  Created by office on 28/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nerby app/nerby_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  nerby_appApp.swift
+//  nerby app
+//
+//  Created by office on 28/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct nerby_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
